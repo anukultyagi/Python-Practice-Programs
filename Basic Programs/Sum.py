6edb48cdfe48b7db6f28f2af0922dbc688113639
anukultyagi <74818876+anukultyagi@users.noreply.github.com>
2022-11-19 22:45:46 +0530

    Basic Python Programs

---
+++ Basic Programs/Sum.py
@@ -0,0 +1,6 @@
+# Python program to add two numbers.
+
+num1 = int(input("Enter First Number:- "))
+num2 = int(input("Enter Second Number:- "))
+sum = str(num1+num2)
+print("The sum of above two number:- "+sum)
